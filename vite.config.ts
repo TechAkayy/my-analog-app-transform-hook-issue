cffe3e4f81afd01e55ec367019fed324510a0e00
Akayy <techakayy@gmail.com>
2024-06-08 22:21:03 +1000

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,34 @@ import analog from '@analogjs/platform';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  plugins: [
+    {
+      enforce: 'pre',
+      name: 'kingfisher-to-puppy',
+      transform: (code, id, options) => {
+        if (id.endsWith('index.page.analog')) {
+          code = code.replace(
+            'https://images.unsplash.com/photo-1444464666168-49d633b86797',
+            'https://images.unsplash.com/photo-1534361960057-19889db9621e'
+          );
+          return { code };
+        }
+      },
+    },
+    analog({
+      vite: {
+        experimental: {
+          supportAnalogFormat: true,
+        },
+      },
+    }),
+  ],
   build: {
     target: ['es2020'],
   },
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog()],
   test: {
     globals: true,
     environment: 'jsdom',
